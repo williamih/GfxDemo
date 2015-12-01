6d8849f7d62d46dd9b20ca2b12ac9746544d2153
William Irwin-Harris <w.irwinharris@gmail.com>
2015-12-01 19:33:44 +1300

    Fix Application object destructor not being called

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -5,19 +5,24 @@
 #  include "Mac/MacApplication.h"
 #endif
 
-static void OnQuit(void*)
+static void OnQuit(void* userdata)
 {
+    Application* app = *(Application**)userdata;
+    delete app;
     OsWindow::QuitEventLoop();
 }
 
 int main()
 {
+    Application* app;
+
 #ifdef __APPLE__
     MacApplication::Initialize();
-    MacApplication::RegisterOnQuit(OnQuit, NULL);
+    MacApplication::RegisterOnQuit(OnQuit, (void*)&app);
 #endif
 
-    Application app;
+    app = new Application;
+
     OsWindow::RunEventLoop();
 
     return 0;
