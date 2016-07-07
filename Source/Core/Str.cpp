9137d23f7c93751ca81726acdb9c20169e33db34
William Irwin-Harris <w.irwinharris@gmail.com>
2016-07-08 10:53:30 +1200

    Add StrPrintf() and StrCmp()

--- Source/Core/Str.cpp
+++ Source/Core/Str.cpp
@@ -1,5 +1,20 @@
 #include "Core/Str.h"
 #include <string.h>
+#include <stdio.h> // for vsnprintf
+
+int StrPrintf(char* dst, size_t dstChars, const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int result = StrPrintfV(dst, dstChars, format, args);
+    va_end(args);
+    return result;
+}
+
+int StrPrintfV(char* dst, size_t dstChars, const char* format, va_list args)
+{
+    return vsnprintf(dst, dstChars, format, args);
+}
 
 void StrCopy(char* dst, size_t dstChars, const char* src)
 {
@@ -36,4 +51,9 @@ size_t StrCopyLen(char* dst, size_t dstChars, const char* src)
 size_t StrLen(const char* str)
 {
     return strlen(str);
+}
+
+int StrCmp(const char* str1, const char* str2)
+{
+    return strcmp(str1, str2);
 }
