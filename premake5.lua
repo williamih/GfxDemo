7372e30d03ba79e6fe79159c2b4c2ba6ed820117
William Irwin-Harris <w.irwinharris@gmail.com>
2016-08-29 10:04:17 +1200

    Convert project to use premake5 as the build system

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+workspace "GfxDemo"
+    configurations { "Debug", "Release" }
+    platforms { "OSX" }
+
+project "GfxDemo"
+    kind "WindowedApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files { "Source/**.h", "Source/**.c", "Source/**.cpp" }
+
+    includedirs "Source"
+
+    filter "configurations:Debug"
+        defines {
+            "DEBUG",
+            "GPUDEVICE_DEBUG_MODE"
+        }
+        flags { "Symbols" }
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+    filter "platforms:OSX"
+        files { "Source/**.m", "Source/**.mm", "**.xib", "Source/Info.plist" }
+        architecture "x64"
+        links {
+            "Cocoa.framework",
+            "Carbon.framework",
+            "Metal.framework",
+            "QuartzCore.framework"
+        }
+        buildoptions { "-std=c++14" }
+        postbuildcommands {
+            'rm -f %{cfg.buildtarget.directory}/Assets',
+            'ln -s "${SRCROOT}/Assets" %{cfg.buildtarget.directory}/Assets',
+        }
+
+    filter "files:**.xib"
+        buildaction "Resource"
+    filter "files:**.plist"
+        buildaction "Resource"
