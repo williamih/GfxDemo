f0cdea35102b54e87d70fea661de0a5b60a48220
William Irwin-Harris <w.irwinharris@gmail.com>
2016-09-04 15:10:14 +1200

    Add PathFindExtension()

--- Source/Core/Path.cpp
+++ Source/Core/Path.cpp
@@ -2,6 +2,17 @@
 #include "Core/Macros.h"
 #include "Core/Str.h"
 
+const char* PathFindExtension(const char* path)
+{
+    size_t len = StrLen(path);
+    const char* p = path + len;
+    for (; p != path && *p != '.'; --p)
+        ;
+    if (p == path)
+        return NULL;
+    return p;
+}
+
 void PathReplaceExtension(char* path, unsigned chars, const char* src,
                           const char* newExtension)
 {
--- Source/Core/Path.h
+++ Source/Core/Path.h
@@ -1,6 +1,10 @@
 #ifndef CORE_PATH_H
 #define CORE_PATH_H
 
+// Returns the file extension of the path (including the dot), or NULL if no
+// file extension was present.
+const char* PathFindExtension(const char* path);
+
 void PathReplaceExtension(char* path, unsigned chars, const char* src,
                           const char* newExtension);
 
