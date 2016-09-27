12290f7e2b950db70bc9cf0e6b7328950fa7b9fd
William Irwin-Harris <w.irwinharris@gmail.com>
2016-09-27 14:24:50 +1300

    Add PathGetProgramDirectory() and PathAppendPath()

--- Source/Core/Path.h
+++ Source/Core/Path.h
@@ -1,6 +1,17 @@
 #ifndef CORE_PATH_H
 #define CORE_PATH_H
 
+const char PATH_USE_OS_SEPARATOR = 0;
+
+void PathGetProgramDirectory(char* dst, unsigned dstChars);
+
+// 'sep' specifies the directory separator to use. This should be either a
+// forward-slash or a backslash.
+// A value of PATH_USE_OS_SEPARATOR can also be specified for 'sep', in which
+// case a backslash will be used on Windows, and a forward-slash on other
+// operating systems.
+bool PathAppendPath(char* dst, unsigned dstChars, const char* src, char sep);
+
 void PathReplaceExtension(char* path, unsigned chars, const char* src,
                           const char* newExtension);
 
